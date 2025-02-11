0a36610802d69a4efa7ed94cd39e2eebc4e6e5a4
FrancescoMarra97 <francescomarra192@gmail.com>
2025-02-11 11:43:11 +0100

    ho spostato tutto in un file

--- Snack1.js
+++ Snack1.js
@@ -1,4 +1,4 @@
-/* Crea una funzione che somma due numeri.
+/* Snack 1:  Crea una funzione che somma due numeri.
 
 Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
 
@@ -17,3 +17,24 @@ const sum = function (a, b) {
 }
 
 const funzioneSomma = (a, b) => a + b;
+
+/* Snack 2: Crea una arrow function che calcola il quadrato di un numero.
+
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga. */
+
+const quadrato = (a) => a * a;
+
+/* Snack 3: Crea una funzione eseguiOperazione
+Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback).
+La funzione deve eseguire l'operazione fornita sui due numeri. */
+
+const sottrazione = (a, b) => a - b;
+const divisione = (a, b) => a / b;
+
+const eseguiOperazione = (a, b, operazione) => operazione(a, b);
+console.log(eseguiOperazione(4, 2, sottrazione));
+console.log(eseguiOperazione(15, 3, divisione));
+
+/* Snack 4:  Crea un generatore di funzioni creaTimer
+Scrivi una funzione creaTimer che accetta un tempo (in ms) 
+e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!". */
